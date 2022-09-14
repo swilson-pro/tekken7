2b4afc0305470391670677f983e396272da0287a
Will <swilson.carlson@hotmail.com>
2022-09-14 16:09:35 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+let fighers = ["bryan", "anna", "alisa", "asuka", "bob", "claudio", "Dragunov", "eddy", "eliza", "feng", "geese", "gigas"]
+
+let div = document.createElement("div")
+const newContent = document
+
+
+const divs = fighter.map(fighter => {
+        document.createElement("div")
+        let img = document.createElement("img")
+        img.
+
+})
+
+const image_url = fighers.map(fighter => {
+    document.createElement(div)
+    return `https://tk7.tekken.com/assets/images/fighters-final/${fighter}-standard/thumbnail-headshot.png`
+})
+
+
+
